181c3bae1b785355ef329c05d9ecf43f554b6ea4
deb <dm3400@srmist.edu.in>
2025-01-11 10:22:07 +0530

    Added help for tool usage

---
+++ src/utils/helpers.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+show_help() {
+	echo "TKLT commands:"
+	echo "  add - task_title task_description priority due_date status  "
+	echo "  list - list  "
+	echo "  delete - delete task_id  "
+	echo "  search - search keyword from title or description  "
+	echo "  update - update column value  "
+	echo "  filter - filter column value  "
+}
